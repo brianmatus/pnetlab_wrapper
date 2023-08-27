e9880069d0c57c4240f8074537439b4068b329fe
brianmatus <35672931+brianmatus@users.noreply.github.com>
2023-08-27 17:37:46 -0600

    Major reformat
    - Adds vnc protocol support via UltraVNC Viewer
    - Reformats code
    - Relocates where the wrapper goes

--- main.cpp
+++ main.cpp
@@ -2,8 +2,7 @@
 #include <fstream>
 #include <vector>
 #include <string>
-
-
+#include <unistd.h>
 
 const char* appDataPath = std::getenv("APPDATA");
 
@@ -11,30 +10,24 @@ std::string filepath = std::string (appDataPath) + R"(\Wireshark\profiles\SSH\pr
 int prefs_host_line = 457;
 int prefs_interface = 485;
 
-
-int main(int argc, char* argv[]) {
-    std::string input(argv[1]);
-    if (input.substr(0, 10) != "capture://") {
-        std::cerr << "Invalid input format. It should start with 'capture://'" << std::endl;
-        return 1;
-    }
+int handleCaptureProtocol(std::string input) {
     input = input.substr(10);
 
     size_t slashPosition = input.find('/');
 
     if (slashPosition == std::string::npos) {
         std::cerr << "Invalid input format. Missing '/' after IP address" << std::endl;
+        sleep(2);
         return 1;
     }
 
     std::string host = input.substr(0, slashPosition);
     std::string interface = input.substr(slashPosition + 1);
 
-
-
     std::fstream theFile(filepath);
     if (!theFile) {
         std::cerr << "Error opening file: " << filepath << std::endl;
+        sleep(2);
         return 1;
     }
 
@@ -45,13 +38,13 @@ int main(int argc, char* argv[]) {
     }
     theFile.close();
 
-
     lines[prefs_host_line - 1] = "extcap.sshdump_exe.remotehost: " + host;
     lines[prefs_interface - 1] = "extcap.sshdump_exe.remoteinterface: " + interface;
 
     std::ofstream outputFile(filepath);
     if (!outputFile) {
         std::cerr << "Error opening file for writing: " << filepath << std::endl;
+        sleep(2);
         return 1;
     }
 
@@ -62,4 +55,34 @@ int main(int argc, char* argv[]) {
 
     const char* cmdCommand = R"("C:\Program Files\Wireshark\Wireshark.exe" -C SSH -i sshdump.exe -k)";
     return system(cmdCommand);
+}
+
+
+int handleVNCProtocol(std::string input) {
+    input = input.substr(6);
+    if (input.back() == '/') {
+        input.pop_back();
+    }
+    std::string cmdCommand = R"("C:\Program Files\uvnc bvba\UltraVNC\vncviewer.exe" )" + input;
+    std::cout << cmdCommand << std::endl;
+    return system(cmdCommand.c_str());
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cout << "Insufficient arguments..." << std::endl;
+        sleep(2);
+        return 1;
+    }
+    std::string input(argv[1]);
+    if (input.find("capture://") != std::string::npos) {
+        return handleCaptureProtocol(input);
+    }
+    if (input.find("vnc://") != std::string::npos) {
+        return handleVNCProtocol(input);
+    }
+
+    std::cerr << "Invalid url handler. It should be in ['capture','vnc']" << std::endl;
+    sleep(3);
+    return 1;
 }
